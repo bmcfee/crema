102648c9867ac25b3392997e485aa5f6b0a99d87
Brian McFee <brian.mcfee@nyu.edu>
2017-05-03 16:38:21 -0400

    checking in start of beat module

---
+++ training/beats/00-setup.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+'''CREMA beats and downbeats'''
+
+import argparse
+import sys
+import os
+import pickle
+
+from tqdm import tqdm
+from joblib import Parallel, delayed
+
+from jams.util import smkdirs
+import muda
+
+import crema.utils
+
+OUTPUT_PATH = 'resources'
+
+
+def process_arguments(args):
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument('--stretch', dest='stretch', type=float, default=0.333,
+                        help='Relative time stretch range')
+
+    parser.add_argument('--n-stretch', dest='n_stretch', type=int, default=4,
+                        help='Number of stretched examples')
+
+    parser.add_argument('--audio-ext', dest='audio_ext', type=str,
+                        default='ogg',
+                        help='Output format for audio (ogg, flac, etc)')
+
+    parser.add_argument('--jams-ext', dest='jams_ext', type=str,
+                        default='jams',
+                        help='Output format for annotations (jams, jamz)')
+
+    parser.add_argument('--jobs', dest='n_jobs', type=int,
+                        default=1,
+                        help='Number of jobs to run in parallel')
+
+    parser.add_argument('input_path', type=str,
+                        help='Path for input (audio, jams) pairs')
+
+    parser.add_argument('output_path', type=str,
+                        help='Path to store augmented data')
+
+    return parser.parse_args(args)
+
+
+def augment(afile, jfile, deformer, outpath, audio_ext, jams_ext):
+    '''Run the data through muda'''
+    jam = muda.load_jam_audio(jfile, afile)
+
+    base = crema.utils.base(afile)
+
+    outfile = os.path.join(outpath, base)
+    for i, jam_out in enumerate(deformer.transform(jam)):
+        muda.save('{}.{}.{}'.format(outfile, i, audio_ext),
+                  '{}.{}.{}'.format(outfile, i, jams_ext),
+                  jam_out, strict=False)
+
+
+def make_muda(stretch, n_stretch):
+    '''Construct a MUDA time stretcher'''
+
+    shifter = muda.deformers.LogspacetimeStretch(n_samples=n_stretch, lower=-stretch, upper=stretch)
+
+    smkdirs(OUTPUT_PATH)
+    with open(os.path.join(OUTPUT_PATH, 'muda.pkl'), 'wb') as fd:
+        pickle.dump(shifter, fd)
+
+    return shifter
+
+
+if __name__ == '__main__':
+    params = process_arguments(sys.argv[1:])
+    smkdirs(OUTPUT_PATH)
+    smkdirs(params.output_path)
+
+    print('{}: setup'.format(__doc__))
+    print(params)
+
+    # Build the deformer
+    deformer = make_muda(params.stretch, params.n_stretch)
+
+    # Get the file list
+    stream = tqdm(crema.utils.get_ann_audio(params.input_path),
+                  desc='Augmenting training data')
+
+    # Launch the job
+    Parallel(n_jobs=params.n_jobs)(delayed(augment)(aud, ann, deformer,
+                                                    params.output_path,
+                                                    params.audio_ext,
+                                                    params.jams_ext)
+                                   for aud, ann in stream)
---
+++ training/beats/01-prepare.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+'''CREMA beats and downbeats'''
+
+import argparse
+import sys
+import os
+import pickle
+
+from tqdm import tqdm
+from joblib import Parallel, delayed
+
+from jams.util import smkdirs
+
+import pumpp
+
+import crema.utils
+
+OUTPUT_PATH = 'resources'
+
+
+def process_arguments(args):
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument('--sample-rate', dest='sr', type=float, default=22050.,
+                        help='Sampling rate for audio analysis')
+
+    parser.add_argument('--hop-length', dest='hop_length', type=int,
+                        default=512,
+                        help='Hop length for audio analysis')
+
+    parser.add_argument('--nfft', dest='n_fft', type=int,
+                        default=2048,
+                        help='Number of FFT bins')
+
+    parser.add_argument('--jobs', dest='n_jobs', type=int,
+                        default=1,
+                        help='Number of jobs to run in parallel')
+
+    parser.add_argument('--augmentation-path', dest='augment_path', type=str,
+                        default=None,
+                        help='Path for augmented data (optional)')
+
+    parser.add_argument('input_path', type=str,
+                        help='Path for directory containing (audio, jams)')
+
+    parser.add_argument('output_path', type=str,
+                        help='Path to store pump output')
+
+    return parser.parse_args(args)
+
+
+def make_pump(sr, hop_length, n_fft):
+    p_feature = pumpp.feature.STFTPhaseDiff(name='stft',
+                                            sr=sr,
+                                            hop_length=hop_length,
+                                            n_fft=n_fft,
+                                            log=True)
+
+    p_beat = pumpp.task.BeatTransformer(name='beat',
+                                        sr=sr, hop_length=hop_length)
+
+    pump = pumpp.Pump(p_feature, p_beat)
+
+    # Save the pump
+
+    with open(os.path.join(OUTPUT_PATH, 'pump.pkl'), 'wb') as fd:
+        pickle.dump(pump, fd)
+
+    return pump
+
+
+def convert(aud, jam, pump, outdir):
+    data = pump.transform(aud, jam)
+    fname = os.path.extsep.join([os.path.join(outdir, crema.utils.base(aud)),
+                                'h5'])
+    crema.utils.save_h5(fname, **data)
+
+
+if __name__ == '__main__':
+    params = process_arguments(sys.argv[1:])
+    smkdirs(OUTPUT_PATH)
+    smkdirs(params.output_path)
+
+    print('{}: pre-processing'.format(__doc__))
+    print(params)
+    pump = make_pump(params.sr, params.hop_length, params.n_fft)
+
+    stream = tqdm(crema.utils.get_ann_audio(params.input_path),
+                  desc='Converting training data')
+    Parallel(n_jobs=params.n_jobs)(delayed(convert)(aud, ann,
+                                                    pump,
+                                                    params.output_path)
+                                   for aud, ann in stream)
+
+    if params.augment_path:
+        stream = tqdm(crema.utils.get_ann_audio(params.augment_path),
+                      desc='Converting augmented data')
+        Parallel(n_jobs=params.n_jobs)(delayed(convert)(aud, ann,
+                                                        pump,
+                                                        params.output_path)
+                                       for aud, ann in stream)
