55ccddb3def2eeddedbb35df3b50314145e4f928
Brian McFee <brian.mcfee@nyu.edu>
2017-05-03 13:43:45 -0400

    switched to short git versions

--- crema/utils.py
+++ crema/utils.py
@@ -33,7 +33,7 @@ def git_version():
         return out
 
     try:
-        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        out = _minimal_ext_cmd(['git', 'rev-parse', '--short', 'HEAD'])
         GIT_REVISION = out.strip().decode('ascii')
     except OSError:
         GIT_REVISION = "Unknown"
