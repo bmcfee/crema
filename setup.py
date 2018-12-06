c0930cd531d4d5e31f919d9fc50f51298579fb5e
Brian McFee <brian.mcfee@nyu.edu>
2018-12-06 14:28:49 -0500

    updating travis configs

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
     ],
